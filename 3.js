c9a03e41498c2c102203cf0949c54e5b8e307fe5
longhaien <longhaien@bigo.sg>
2021-05-20 21:13:00 +0800

    3.js

---
+++ 3.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function (str) {
+  if (!str.length) return 0;
+  const subStrArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    subStrArr[i] = [str[i]];
+    for (let j = i + 1; j < str.length; j++) {
+      if (!subStrArr[i].includes(str[j])) {
+        subStrArr[i].push(str[j]);
+      } else {
+        break;
+      }
+    }
+  }
+  const _subStrArr = subStrArr.sort((a, b) => {
+    return b.length - a.length
+  })
+  return _subStrArr[0].length;
+};
