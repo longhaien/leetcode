c7573db88618bd7334c54520bd0aa2829002dd62
longhaien <longhaien@bigo.sg>
2021-06-07 09:58:34 +0800

    quickSort

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@
 var twoSum = function (nums, target) {
   let result = [];
 
-  // 弄个Map减速，加快检索速度
+  // 弄个Map检索，加快检索速度
   const numsMap = {};
   for (let i = 0; i < nums.length; i++) {
     const curr = nums[i];
